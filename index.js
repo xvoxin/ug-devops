1825385e49e07ad52bbb4452ae182cec286f7d7e
xvoxin <michalniskowski0@gmail.com>
2020-04-05 10:07:27 +0200

    add nwd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const redis = require("redis");
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
 
 const app = express();
 
@@ -8,9 +9,37 @@ const client = redis.createClient({
     port: 6379
 });
 
+client.set('nwd', 'n/a');
+
 app.get('/', (req, res) => {
-    res.send("Hello from my docker webapp guys! Again!");
+    client.get('counter', (err, counterValue) => {
+        var currentValue = parseInt(counterValue) + 1;
+        client.set('counter', currentValue);
+        res.send("Hello " + currentValue + " time!");
+    })
 });
+
+app.get('/nwd/:l1&:l2', (req, res) => {
+    var result = NWD(req.params.l1, req.params.l2)
+    res.send("Nwd " + result);
+    client.set('nwd', result);
+});
+
+app.get('/nwd', (req, res) => {
+    client.get('nwd', (err, nwd) => {
+        res.send("Cached nwd " + nwd);
+    })
+});
+
+function NWD(a,b){
+  var q;
+  while (b != 0){
+    q = a;
+    a = b;
+    b = q % b;
+ }
+ return a;
+}
 
 app.listen(8080, () => {
     console.log("Listening on port 8080");
