bb37478bf42fd080af6f8a626ed1a72a44c26124
xvoxin <michalniskowski0@gmail.com>
2020-06-19 19:23:17 +0200

    move kubernetes scripts to kube directory

--- lab10/start.sh
+++ lab10/start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f myapp-configmap.yml
-kubectl apply -f redis-deployment.yml
-kubectl apply -f redis-service-clusterip.yml
-kubectl apply -f postgres-secret.yml
-kubectl apply -f postgres-pvc.yml
-kubectl apply -f postgres-deployment.yml
-kubectl apply -f postgres-service-clusterip.yml
-kubectl apply -f mybackend-deployment.yml
-kubectl apply -f mybackend-service-clusterip.yml
-kubectl apply -f myfrontend-deployment.yml
-kubectl apply -f myfrontend-service-clusterip.yml
-kubectl apply -f ingress-service.yml
+kubectl apply -f kube/myapp-configmap.yml
+kubectl apply -f kube/redis-deployment.yml
+kubectl apply -f kube/redis-service-clusterip.yml
+kubectl apply -f kube/postgres-secret.yml
+kubectl apply -f kube/postgres-pvc.yml
+kubectl apply -f kube/postgres-deployment.yml
+kubectl apply -f kube/postgres-service-clusterip.yml
+kubectl apply -f kube/mybackend-deployment.yml
+kubectl apply -f kube/mybackend-service-clusterip.yml
+kubectl apply -f kube/myfrontend-deployment.yml
+kubectl apply -f kube/myfrontend-service-clusterip.yml
+kubectl apply -f kube/ingress-service.yml
